8441aa902f72bf9ca705f59839c534c549f34d02
Michael Gulley <mgulley.mograph@gmail.com>
2017-05-21 19:07:50 -0400

    Recipe class defined.

--- src/com/lumatik/app/Main.java
+++ src/com/lumatik/app/Main.java
@@ -1,12 +1,40 @@
 package com.lumatik.app;
 
+import com.lumatik.model.Difficulty;
+import com.lumatik.model.Rating;
+import com.lumatik.model.Recipe;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by mikejgulley on 5/21/2017.
  */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Meow!");
+	    String testRecipeName = "Test Recipe";
+
+        List<String> ingredients = new ArrayList<>();
+	    ingredients.add("Chicken");
+        ingredients.add("Tomatoes");
+        ingredients.add("Basil");
+        ingredients.add("Provolone Cheese");
+        ingredients.add("Olive Oil");
+
+        List<String> directions = new ArrayList<>();
+        directions.add("Grill chicken");
+        directions.add("Cover with Cheese");
+        directions.add("Eat dat food, mang!");
+
+        Recipe testRecipe = new Recipe(0, testRecipeName, ingredients, directions, LocalDate.now());
+
+        testRecipe.setDescription("Super nom nom Chicken Recipe!");
+        testRecipe.setGenre("Italian");
+        testRecipe.setRating(Rating.FIVE_STARS);
+        testRecipe.setDifficulty(Difficulty.MEDIUM);
+
+        System.out.println(testRecipe);
     }
 }
--- src/com/lumatik/model/Recipe.java
+++ src/com/lumatik/model/Recipe.java
@@ -19,9 +19,182 @@ public class Recipe {
     private Difficulty difficulty;
     private boolean isFavorite;
     private List<String> pairWithOptions;
+    private List<String> notes;
     private String pictureUrl;
     private String videoUrl;
     private LocalDate createDate;
     private LocalDate lastMadeDate;
     private LocalDate updateDate;
+
+    public Recipe(int id, String name, List<String> ingredients, List<String> directions, LocalDate createDate) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.directions = directions;
+        this.createDate = createDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getAltIngredients() {
+        return altIngredients;
+    }
+
+    public void setAltIngredients(List<String> altIngredients) {
+        this.altIngredients = altIngredients;
+    }
+
+    public List<String> getCookware() {
+        return cookware;
+    }
+
+    public void setCookware(List<String> cookware) {
+        this.cookware = cookware;
+    }
+
+    public List<String> getDirections() {
+        return directions;
+    }
+
+    public void setDirections(List<String> directions) {
+        this.directions = directions;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public List<String> getPairWithOptions() {
+        return pairWithOptions;
+    }
+
+    public void setPairWithOptions(List<String> pairWithOptions) {
+        this.pairWithOptions = pairWithOptions;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+//    public void setCreateDate(LocalDate createDate) {
+//        this.createDate = createDate;
+//    }
+
+    public LocalDate getLastMadeDate() {
+        return lastMadeDate;
+    }
+
+    public void setLastMadeDate(LocalDate lastMadeDate) {
+        this.lastMadeDate = lastMadeDate;
+    }
+
+    public LocalDate getUpdateDate() {
+        return updateDate;
+    }
+
+//    public void setUpdateDate(LocalDate updateDate) {
+//        this.updateDate = updateDate;
+//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Recipe recipe = (Recipe) o;
+
+        if (getId() != recipe.getId()) return false;
+        if (!getName().equals(recipe.getName())) return false;
+        return getCreateDate().equals(recipe.getCreateDate());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + getName().hashCode();
+        result = 31 * result + getCreateDate().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", genre='" + genre + '\'' +
+                ", rating=" + rating +
+                ", difficulty=" + difficulty +
+                ", createDate=" + createDate +
+                '}';
+    }
 }
